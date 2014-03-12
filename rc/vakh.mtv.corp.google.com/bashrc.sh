a1ae8145b8de99055fd5b97526e692c448a695be
Varun Khaneja <khaneja+github@gmail.com>
2014-03-12 15:13:22 -0700

    Updating the machine-specific bashrc to also source the machine specific alias and functions

--- rc/vakh.mtv.corp.google.com/bashrc.sh
+++ rc/vakh.mtv.corp.google.com/bashrc.sh
@@ -5,7 +5,14 @@ function run
   local MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   local GOOG_DIR="${MY_DIR}/../goog"
   local GOOG_BASH_RC="${GOOG_DIR}/bashrc.sh"
+
   [ -f "${GOOG_BASH_RC}" ] && source "${GOOG_BASH_RC}"
+
+  local FUNCTIONS_FILE="${MY_DIR}/functions.sh"
+  [ -f "${FUNCTIONS_FILE}" ] && source "${FUNCTIONS_FILE}"
+
+  local ALIAS_FILE="${MY_DIR}/alias.sh"
+  [ -f "${ALIAS_FILE}" ] && source "${ALIAS_FILE}"
 }
 
 run
