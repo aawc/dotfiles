4fca96ff961215fb83ba57c5a5a4719e6ea655e3
Varun Khaneja <ca.aawc@yahoo.com>
2014-05-05 18:53:15 -0700

    Show the currently running screens and offer to start a new one if there's none matching the provided name

--- rc/common/functions.sh
+++ rc/common/functions.sh
@@ -10,7 +10,16 @@ function rescreen
 
   local intendedScreenName="$(screen -ls | grep "${screenName}" | awk '{print $1}')"
   if [ -z "${intendedScreenName}" ]; then
-    printf "Screen %s not found\n" $screenName
+    printf "Screen %s not found.\n" $screenName
+    printf "$(screen -ls)"
+    local startScreen="N"
+    read -r -p "Want to start a session named '${screenName}'? [y/N]: " startScreen
+    case "${startScreen}" in
+      [yY][eE][sS]|[yY])
+        screen -S "${screenName}";;
+      *)
+        ;;
+    esac
   else
     local numberOfMatchingScreensFound="$(echo ${intendedScreenName} | wc -w)"
     if [ "${numberOfMatchingScreensFound}" -eq 1 ]; then
