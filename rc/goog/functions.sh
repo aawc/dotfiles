356ab269fdf5c22bee1caacf99c41c3d89e41a26
Varun Khaneja <ca.aawc@yahoo.com>
2014-10-17 13:37:26 -0700

    Adding a function to clear out git based g4 clients

--- rc/goog/functions.sh
+++ rc/goog/functions.sh
@@ -23,3 +23,20 @@ function prodaccess_missing_message
 {
   echo "(prodaccess missing)"
 }
+
+function git_cleanup_clients
+{
+  local clients=$(g4 clients -u "${USER}" -e '*git*' | awk '{print $2}' | cut -d ':' -f 2);
+  for client in ${clients}; do
+    cd "${HOME}";
+    echo "Client: ${client}";
+    g4d "${client}";
+    local pending_cls=$(g4 pending | grep Change | grep -o '[0-9]\{8,\}');
+    for pending_cl in ${pending_cls}; do
+      echo "CL: ${pending_cl}";
+      echo "g4 revert -c ${pending_cl}";
+    done
+    echo "g4 citc -d ${client}";
+    echo;
+  done
+}
