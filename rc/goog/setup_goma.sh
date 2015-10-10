37922317c8b7b75a9b57c36351479e6b4aa30fed
Varun Khaneja <ca.aawc@yahoo.com>
2015-10-10 11:48:51 -0700

    export GOMA_DIR, and add instructions for running Chrome and unittests

--- rc/goog/setup_goma.sh
+++ rc/goog/setup_goma.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 GOMA_DIR=${HOME}/goma
-mkdir ${GOMA_DIR}
-cd ${GOMA_DIR}
+mkdir -p ${GOMA_DIR}
+pushd ${GOMA_DIR}
 curl https://clients5.google.com/cxx-compiler-service/download/goma_ctl.py -o goma_ctl.py
 python goma_ctl.py update
+popd
