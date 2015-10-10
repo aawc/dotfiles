f84ed7c54a8770ceaf63a4fb0f176594ea75490d
Varun Khaneja <ca.aawc@yahoo.com>
2015-10-10 11:48:51 -0700

    export GOMA_DIR, and add instructions for running Chrome and unittests

--- rc/goog/use_goma.sh
+++ rc/goog/use_goma.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "Assuming this is run from src/ directory"
-GOMA_DIR=${HOME}/goma
+export GOMA_DIR=${HOME}/goma
 python ${GOMA_DIR}/goma_ctl.py ensure_start
-GYP_GENERATORS=ninja ./build/gyp_chromium -D use_goma=1
+export GYP_GENERATORS=ninja && ./build/gyp_chromium -D use_goma=1
 echo "Now go use ninja build mechanism!"
+
+echo "\# For build and run:"
+echo "nice ninja -C out/Debug chrome -j 100 && " \
+  "DISPLAY=:20.0 out/Debug/chrome --user-data-dir=/tmp/profile"
+
+echo "\# For unit tests:"
+echo "ninja -C out/Debug -j 100 unit_tests && " \
+  "out/Debug/unit_tests --gtest_filter=\"\*Mytest.Thing\*\""
