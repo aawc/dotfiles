02f1828d8cb962054c3f8afc40f5dfe643c4e67d
Varun Khaneja <ca.aawc@yahoo.com>
2016-04-14 14:17:19 -0700

    Add hex2dec, remove youtube-dl functions because they don't work anymore and I wasn't using them anyway

--- rc/common/functions.sh
+++ rc/common/functions.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function hex2dec
+{
+  if [ $# -ne 1 ]; then
+    printf "No/too-many arguments: %d\n" $#
+    return
+  fi
+  local input="${1}"
+  local input_no_whitespace="$(echo -e "${input}" | tr -d '[[:space:]]')"
+  printf "%d\n" "0x${input_no_whitespace}"
+}
+
 function rescreen
 {
   local screenName="${1}"
@@ -104,31 +115,6 @@ function source_file() {
   if [[ -f "${file_to_source}" ]]; then
     source "${file_to_source}"
   fi
-}
-
-function play {
-    # Skip DASH manifest for speed purposes. This might actually disable
-    # being able to specify things like 'bestaudio' as the requested format,
-    # but try anyway.
-    # Get the best audio that isn't WebM, because afplay doesn't support it.
-    # Use "$*" so that quoting the requested song isn't necessary.
-    youtube-dl --default-search=ytsearch: \
-               --youtube-skip-dash-manifest \
-               --output="${TMPDIR:-/tmp/}%(title)s-%(id)s.%(ext)s" \
-               --restrict-filenames \
-               --format="bestaudio[ext!=webm]" \
-               --exec=afplay "$*"
-}
-
-function mp3 {
-    # Get the best audio, convert it to MP3, and save it to the current
-    # directory.
-    youtube-dl --default-search=ytsearch: \
-               --restrict-filenames \
-               --format=bestaudio \
-               --extract-audio \
-               --audio-format=mp3 \
-               --audio-quality=1 "$*"
 }
 
 function goto()
