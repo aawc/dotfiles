8d71a923aa15c30322479263f930dedc440b0bad
Varun Khaneja <ca.aawc@yahoo.com>
2016-08-17 16:53:54 -0700

    Add abbr for reloading config and verbose rf

--- .config/fish/abbrs.fish
+++ .config/fish/abbrs.fish
@@ -1,5 +1,6 @@
 abbr -a g 'git'
 abbr -a gco 'git checkout'
-abbr -a rf 'rm -rfI'
+abbr -a rf 'rm -rfIv'
+abbr -a rl 'source ~/.config/fish/config.fish'
 abbr -a s 'ls'
 abbr -a vv 'git branches -vv'
