b75fe28b8140675b131e130d069fe1b630519c60
Varun Khaneja <ca.aawc@yahoo.com>
2016-08-23 14:59:39 -0700

    Add a function to show the contents of various compressed file formats

---
+++ .config/fish/functions/show_archive.fish
@@ -0,0 +1,30 @@
+function show_archive --description "List contents of compressed files"
+  for file in $argv
+    if test -f $file
+      echo -s "Listing: " (set_color --bold blue) $file (set_color normal)
+      switch $file
+        case *.tar
+          tar -tf $file
+        case *.tar.bz2 *.tbz2
+          bunzip2 -c $file | tar -tf - --
+        case *.tar.gz *.tgz
+          tar -ztf $file
+        case *.zip *.ZIP
+          unzip -l $file
+        case *.bz2
+          bunzip2 $file
+          # Can also use: bzip2 -d $file
+        case *.gz
+          gunzip $file
+        case *.rar
+          unrar x $file
+        case *.pax
+          pax -r < $file
+        case '*'
+          echo "Extension not recognized, cannot list contents of $file"
+      end
+    else
+      echo "$file is not a valid file"
+    end
+  end
+end
