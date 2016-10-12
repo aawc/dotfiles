4df5f9ce14b84850f46b894f473f0985b397f153
Varun Khaneja <ca.aawc@yahoo.com>
2016-10-12 14:32:01 -0700

    Improve the custom diff script to handle the case of CLI-only environments

--- rc/common/diff.py
+++ rc/common/diff.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python
 
-import sys
 import os
+import platform
+import subprocess
+import sys
 
-os.system('meld "%s" "%s"' % (sys.argv[2], sys.argv[5]))
+
+def gui_main():
+  os.system('meld "%s" "%s"' % (sys.argv[2], sys.argv[5]))
+
+
+def cli_main():
+  command = 'colordiff "%s" "%s"' % (sys.argv[2], sys.argv[5])
+  os.system(command);
+
+
+def X_is_running():
+  pp = subprocess.Popen(
+      ["xset", "-q"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  pp.communicate()
+  return pp.returncode == 0
+
+
+def current_platform():
+  return platform.system()
+
+
+if __name__ == '__main__':
+  if current_platform() == "Linux" and X_is_running():
+    gui_main()
+  else:
+    cli_main()
