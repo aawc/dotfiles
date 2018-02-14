e0a58b5c52ddf0ed9fffa3b2d5110de4c924884a
Varun Khaneja <ca.aawc@yahoo.com>
2018-02-14 15:34:26 -0800

    Use only ASCII chars for git status line

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,9 +16,13 @@ set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stagedstate '->'
 set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_untrackedfiles 'UT'
 set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_stashstate 'ST'
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
