bf755bd3de55740dfcb98dac7baaccd863ba7285
Varun Khaneja <git.bin@khaneja.org>
2022-04-22 16:34:45 -0700

    Improve scripts to sync and build chromium

--- misc/scripts/buildChromium.sh
+++ misc/scripts/buildChromium.sh
@@ -1,9 +1,21 @@
-#!/bin/bash -i
+#!/bin/bash
 
-rm -f /tmp/compiling.txt;
+set -x;
+
+export __LATEST__=$(date +%s)
+export LOG_FILE="/tmp/build-chromium-src-${__LATEST__}.log"
+rm -f ${LOG_FILE};
+
+export PATH="${HOME}/work/chrome/depot_tools:${PATH}"
+echo $PATH >> ${LOG_FILE} 2>&1
+
+export __BUILD__="ReleaseWithSymbols"
+
 cd "${HOME}/work/chrome/src" &&
-  pwd >>/tmp/compiling.txt 2>&1 &&
-  nice gn check out/gn_out >>/tmp/compiling.txt 2>&1 &&
-  nice ninja -C out/gn_out -j 10 components/safe_browsing_db:safe_browsing_db >>/tmp/compiling.txt 2>&1 &&
-  nice ninja -C out/gn_out -j 10 components_unittests >>/tmp/compiling.txt 2>&1 &&
-  nice ninja -C out/gn_out -j 10 browser_tests >>/tmp/compiling.txt 2>&1
+  pwd >> ${LOG_FILE} 2>&1 &&
+  nice gn check out/${__BUILD__} >> ${LOG_FILE} 2>&1 &&
+  nice autoninja -C out/${__BUILD__} -k 0 chrome >> ${LOG_FILE} 2>&1 &&
+  nice autoninja -C out/${__BUILD__} -k 0 base_unittests components_unittests chrome/test:unit_tests browser_tests unit_tests >> ${LOG_FILE} 2>&1
+  nice echo "Done!" >> ${LOG_FILE} 2>&1
+
+set +x;
